4889bca9aa22ef02ddb42b4a165687686bbefd0a
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-04-19 07:31:37 +0100

    first commit

---
+++ src/main/java/com/example/saadiq/photos/clone/PhotosController.java
@@ -0,0 +1,47 @@
+package com.example.saadiq.photos.clone;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class PhotosController {
+
+    private Map<String, Photo> db = new HashMap<>(){{
+        put("1", new Photo("1", "hello.jpg"));
+    }};
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/photos")
+    public Collection<Photo> get() {
+        return db.values();
+    }
+
+    @GetMapping("/photos/{id}")
+    public Photo get(@PathVariable String id) {
+        Photo photo = db.get(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
+    }
+
+    @DeleteMapping("/photos/{id}")
+    public void delete(@PathVariable String id) {
+        Photo photo = db.remove(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/photos/")
+    public void create(Photo photo) {
+        photo.setId(UUID.randomUUID().toString());  // generating a random number
+        
+    }
+}
